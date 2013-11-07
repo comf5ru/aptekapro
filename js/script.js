bd1c917503cdf0fa9197ecf375026f0b459fab4c
Александр Мыгаков <mgk@f5.com.ru>
2013-11-07 12:19:11 +0400

    карусель левая

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+$('.carousel').carousel();
+
+
 $(".row.tovari .col-md-2").hover(function() {
 	$(this).find(".animate-btn").stop(true).animate({"bottom":"0"})
 }, function() {
