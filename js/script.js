044324aa3a42859d8bd5e0295b92f939bb1ed6b3
Александр Мыгаков <mgk@f5.com.ru>
2013-11-11 17:47:51 +0400

    главная

--- js/script.js
+++ js/script.js
@@ -8,6 +8,19 @@ $(".row.tovari .tovarcol").hover(function() {
 }, function() {
 	$(this).find(".animate-btn").stop(true).animate({"bottom":"-68px"})
 });
+
+console.log('123');
+
+
+$(".poisk input").focus(function(){
+console.log('23');
+	$(".vipod").show();
+})
+
+$(".poisk input").blur(function(){
+	$(".vipod").hide();
+})
+
 	
 	
 });
